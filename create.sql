362d3fbfab4c8e69501a9aff60a9432bc6625ad9
Alisson Rodrigues <alisson@tivic.com.br>
2024-05-20 20:33:13 -0300

    initial commit

---
+++ create.sql
@@ -0,0 +1,13 @@
+drop schema branas_cc cascade;
+
+create schema branas_cc;
+
+create table branas_cc.account (
+	account_id uuid primary key,
+	name text not null,
+	email text not null,
+	cpf text not null,
+	car_plate text null,
+	is_passenger boolean not null default false,
+	is_driver boolean not null default false
+);
